68e0ee3e2b07e050f3c17b41e57c3d5c1e60da71
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-27 19:51:22 +0000

    Automatically backed up by Learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -2,5 +2,8 @@ class SongsWorker
   include Sidekiq::Worker
 
   def perform(songs_file)
-  end 
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+  end
 end
